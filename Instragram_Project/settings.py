2c8ea96691c642fcbc866e6b9d5f06674c0e8493
shakiblimon <shakiblimon@circlefintech.com>
2019-02-13 15:16:23 +0600

    settings , admin updated

--- Instragram_Project/settings.py
+++ Instragram_Project/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#Configuring Media Root Folder
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
