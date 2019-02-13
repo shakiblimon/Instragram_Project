67759f3f29fce60cff81b3abb85040a0d02b8c6e
shakiblimon <shakiblimon@circlefintech.com>
2019-02-13 15:22:07 +0600

    Home page view added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from posts.models import Post
+
+
+class HomePageView(ListView):
+     model = Post
+     template_name = 'home.html'
