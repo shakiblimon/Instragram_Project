a61b44f9086621ea9342cc7365e9c64782c6e007
shakiblimon <shakiblimon@circlefintech.com>
2019-02-13 14:39:59 +0600

    Model add and updated

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.TextField(())
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        """
+        :return:
+        """
+        return self.title
